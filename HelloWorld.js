fe991d31f6d11236ac358e0dc7dcb9bdba55af1c
unknown <bahloulmubarik786@gmail.com>
2025-11-10 10:30:46 +0500

    tut

---
+++ HelloWorld.js
@@ -0,0 +1,56 @@
+// let i = 1;
+// while(i<=5){
+//     console.log(i);
+//     i++;
+// }
+
+// let i=1;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=15);
+
+// let password;
+
+// while(password!=="1234"){
+//     password= prompt("Enter your password:");}
+
+// console.log("Access granted!");
+
+//for in-loop
+
+// let student = {
+//   name : "Bahloul",
+//   age : 24,
+//   city : "Islamabad",
+//   cgpa : 3.8,
+//   ispassed : true
+// };
+
+// for (let key in student){
+//     console.log("key =", key, "value", student[key]);
+// }
+
+//print even numbers from 0 to 100
+
+// for (let i=0; i<=100; i++){
+//     if(i%2===0){
+//         console.log(i);
+//     }
+// }
+
+//guess the number game
+
+// let secretNumber = 7;
+
+// let usernum = prompt("Guess the secret number"); //user will enter prompt to guess number
+
+// while(usernum != secretNumber){
+//     usernum = prompt("Wrong guess! Try again.");
+// }
+
+// console.log("Congratulations! You guessed the secret number.");
+
+let str = "Bahloul";
+
+console.log(str.length);
