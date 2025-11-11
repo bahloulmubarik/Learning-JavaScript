f0f6c2d8e59a12ae63aad4b880dcc32a676058e7
unknown <bahloulmubarik786@gmail.com>
2025-11-11 07:04:59 +0500

    arrays

--- HelloWorld.js
+++ HelloWorld.js
@@ -51,6 +51,33 @@
 
 // console.log("Congratulations! You guessed the secret number.");
 
-let str = "Bahloul";
+// let str = "Bahloul";
 
-console.log(str.length);
+// console.log(str.length);
+
+
+//string question
+
+// let fullName = prompt("Enter your full name");
+// let userName = "@" + fullName + fullName.length;
+// console.log(userName);
+ 
+//arrays
+
+// let marks = [12,24,43,53,64];
+// console.log(marks);
+
+// let Heros = ["Krish","superman","bahloul","spiderman","batman","ironman","captain america","thor","hulk"];
+
+// for (let idx=0; idx<Heros.length; idx++){
+//     console.log(Heros[idx]);
+// }()
+
+let marks = [12,24,43,53,64]; //sum and average of array elements
+let sum = 0; //initialize sum to 0
+
+for(let val of marks){ //for each loop
+    sum += val; //add each element to sum
+}
+let avg = sum / marks.length;  //calculate average by dividing sum by number of elements in array 
+console.log(`The average is: ${avg}`); //print average to console 
