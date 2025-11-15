324d7ae72a46542eba77ea1c93db247a71a5de53
unknown <bahloulmubarik786@gmail.com>
2025-11-15 21:40:26 +0500

    new updates

--- test.js
+++ test.js
@@ -1,8 +1,1 @@
-const accountId = 786 
-let accountEmail = "Bahloulmubarik786@gmail.com"
-accountCity = "Islamabad"
-accountEmail = "hcdhsl@g.com"
-accountPassword = "234928374"
-let accountState
-console.log(accountId)
-console.table([accountId, accountEmail, accountCity, accountState])
+console.log("Hello, Bahloul!");
